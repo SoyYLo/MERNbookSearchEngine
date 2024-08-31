59c572f347354b01f7166f6c1d6cabe5cc2a58ba
Yahvannel <yahvannellopez@gmail.com>
2024-08-31 15:46:00 -0500

    completed server sside code

---
+++ server/schema/resolvers.js
@@ -0,0 +1,72 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+  
+
+
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).select('-__v -password')
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    signup: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async (parent, { bookInput }, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { savedBooks: bookInput },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw AuthenticationError;
+    },
+    // Set up mutation so a logged in user can only remove their profile and no one else's
+    removeBook: async (parent, {bookId}, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate({ _id: context.user._id },
+            {$pull:{savedBooks: {bookId}}}
+        );
+      }
+      throw AuthenticationError;
+    },    
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schema/typedefs.js
@@ -0,0 +1,46 @@
+const typeDefs = `
+type Book{
+  authors:[String]
+  description:String
+  bookId: ID!
+  image: String
+  link: String
+  title: String
+}
+
+type User{
+_id:ID!
+username: String
+email: String
+password: String
+savedBooks: [Book]
+}
+
+type Auth{
+user:User
+token:ID!
+}
+
+type Query{
+me:User
+}
+
+input BookInput{
+ authors:[String]
+  description:String
+  bookId: String!
+  image: String
+  link: String
+  title: String
+}
+
+type Mutation{
+signup(username: String!, email: String!, password: String!): Auth
+login(email: String!, password: String!): Auth
+saveBook(bookInput: BookInput):User
+removeBook(bookId: ID!): User
+}
+
+`
+
+module.exports = typeDefs;
