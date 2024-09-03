027ca5ec62710652e14f6f4a9d613343091e0865
Yahvannel <yahvannellopez@gmail.com>
2024-09-03 18:16:29 -0500

    replaced loginUser() with LOGIN_USER mutation

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/clint';
 import { ADD_USER } from '../utils/mutations';
-import { createUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
